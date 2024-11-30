fb283a2dfc62803c4e6ca5d2e6bb6db4885ae78a
marialuiza1324 <marialuiza.nerypinto@gmail.com>
2024-11-30 19:01:46 -0300

    melhorias nas views

--- public/js/users/menu.js
+++ public/js/users/menu.js
@@ -5,14 +5,13 @@ document.addEventListener("DOMContentLoaded", function(){
     // ao clicar no botão de abrir o menu ele abre o menu e some o svg
     abrirMenu.addEventListener("click", function(event){
         navbar.classList.remove("hidden")
-        navbar.classList.add("absolute", "top-12", "right-0", "px-2", "py-4", "rounded-md", "flex", "flex-col", "border", "bg-black")
-        // abrirMenu.classList.add("hidden")
+        navbar.classList.add("absolute", "top-12", "right-0", "px-2", "py-4", "rounded-md", "flex", "flex-col", "border", "bg-gray-950")
         event.stopPropagation()
     })
 
     // ao clicar fora do botão de abrir o menu ou de um do seus filhos, ele fecha
     document.addEventListener("click", function(event){
-        if (!abrirMenu.contains(event.target)) {
+        if (!navbar.contains(event.target)) {
             navbar.classList.add("hidden")
             abrirMenu.classList.remove("hidden")
         }
