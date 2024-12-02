022d944c8517aba906659c75978154bd9eb3e34b
Maria Eduarda de Oliveira Furini <129913960+MaduFurini@users.noreply.github.com>
2024-12-01 22:33:49 -0300

    Removed comments

--- public/js/home/shop.js
+++ public/js/home/shop.js
@@ -22,28 +22,17 @@ let selectedId = null;
 
 document.querySelectorAll('.variacao').forEach(item => {
     item.addEventListener('click', function () {
-        // Obtém o ID e o nome da variação clicada
         const idVarioacao = item.getAttribute('data-id');
         const nomeVarioacao = item.getAttribute('data-nome');
 
-        // Atualiza o texto exibido com o nome da variação
         document.getElementById('selected-name').textContent = `Variação Selecionada: ${nomeVarioacao}`;
 
-        // Armazena o ID da variação para uso posterior
         selectedId = idVarioacao;
 
-        // Adiciona a classe 'selected' à variação clicada para mudar a cor
         document.querySelectorAll('.variacao').forEach(variacao => {
-            variacao.classList.remove('selected'); // Remove a seleção de outras variações
+            variacao.classList.remove('selected');
         });
 
-        item.classList.add('selected'); // Adiciona a classe 'selected' à variação clicada
-
-        // Log para verificar se o ID foi armazenado
-        console.log('ID da variação selecionada:', selectedId);
+        item.classList.add('selected');
     });
 });
-
-function changeImage(src) {
-    document.getElementById('selected-image').src = src;
-}
